b32318de66de6754e93fb6cdb9b8e47236c8185a
Arturo <alopez96@ucsc.edu>
2019-01-12 18:46:58 -0800

    retrieving weather info

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Weather from './components/Weather';
+import { API_KEY } from './utils/WeatherAPIKey';
+
 
 export default class App extends React.Component {
+  state = {
+    isLoading: false,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Error Getting Weather Condtions'
+        });
+      }
+    );
+  }
+
+  fetchWeather(lat = 25, lon = 25) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=imperial`
+    )
+      .then(res => res.json())
+      .then(json => {
+        // console.log(json);
+        this.setState({
+          temperature: json.main.temp,
+          weatherCondition: json.weather[0].main,
+          isLoading: false
+        });
+      });
+  }
+
   render() {
+    const {isLoading, weatherCondition, temperature} = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      {isLoading ? ( 
+        <Text>Fetching weather data</Text>
+      ):(
+        <View>
+          <Weather weather={weatherCondition} temperature={temperature}></Weather>
+        </View>
+      )}
       </View>
     );
   }
---
+++ utils/WeatherAPIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = '124937c31ac5b9be3ed8666a4de85528';
